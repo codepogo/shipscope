b28cd5e334c3695bce0dcdfd5e878bcb86d91f91
David McGaffin <david@codeship.io>
2014-09-29 16:29:46 -0400

    deverbosify the build status message.
    also bump the version to 0.9.8 in anticipation of the next release.

--- app/js/util/build_watcher.js
+++ app/js/util/build_watcher.js
@@ -10,22 +10,14 @@ var BuildWatcher = (function() {
     }
 
     showNotification = function(project, build) {
-      var msg
-      if (build.status == 'success') {
-        msg = '[' + build.branch + '] succeeded.\n' +  ellipsify(build.message, 30)
-      } else if (build.status == 'error') {
-        msg = '[' + build.branch + '] failed.\n' + ellipsify(build.message, 30)
-      } else {
-        msg = '[' + build.branch + '] was stopped.\n' + ellipsify(build.message, 30)
-      }
-
-      var options = {
-        type: "basic",
-        title: "shipscope: " + project.repository_name,
-        message: msg,
-        priority: 1,
-        iconUrl: "img/shipscope_icon_" + build.status + "_128.png"
-      };
+      var msg= '[' + ellipsify(build.branch, 20) + '] ' + build.status + '\n' + ellipsify(build.message, 30),
+          options = {
+            type: "basic",
+            title: project.repository_name,
+            message: msg,
+            priority: 1,
+            iconUrl: "img/shipscope_icon_" + build.status + "_128.png"
+          };
       chrome.notifications.create(build.uuid, options, function() {});
     }
 
