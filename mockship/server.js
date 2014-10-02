065ce793c05b7f728b439418706b37596efabbd2
David McGaffin <david@codeship.io>
2014-10-02 09:05:31 -0400

    initial, BROKEN, implementation of mock server
    I either need to change the server to run on https so Chrome is happy
    making requests to it, or I need to change the extension permissions so
    it can make requests to http urls.
    
    I think it would be much better to either:
    
    1. setup a nginx https -> http proxy so I can make https requests to the
    http backend.
    1. create a certificate for a dev instance of mothership. Also use nginx
    for this. The benefit here is that we get https/nginx setup for
    mothership.

--- mockship/server.js
+++ mockship/server.js
@@ -1,8 +1,19 @@
-projects = require('./data.js').projects
-express = require('express')
+var projects = require('./data.js').projects,
+    express = require('express')
 
 /**
  * 1. open server
  * 2. respond to requests with project
  */
 
+var app = express(),
+    server = app.listen(3030, function() {
+      console.log('mockserver is ready to go on port 3030')
+    })
+
+app.get('/', function(req, res) {
+  res.type('application/json').send(projects)
+})
+app.get('/projects.json', function(req, res) {
+  res.type('application/json').send(projects)
+})
