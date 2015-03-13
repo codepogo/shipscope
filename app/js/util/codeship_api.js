539d0f3eb4d1e1ad9a2e7f4aa6ac72486da89c7f
beanieboi <beanie@benle.de>
2015-03-13 09:45:23 +0100

    refactor fetchApiKey into CodeshipApi

--- app/js/util/codeship_api.js
+++ app/js/util/codeship_api.js
@@ -5,45 +5,52 @@ var CodeshipApi = (function() {
 
     options,
 
-    fetchBuilds = function(project) {
-      var params = 'api_key=' + options.api_key + "&project_id=" + project.id
+    fetchBuilds = function(project, callback) {
+      fetchApiKeyFromLocalStorage(function(){
+        var params = 'api_key=' + options.api_key + "&project_id=" + project.id
 
-      $.getJSON(BUILD_URL, params)
-        .done( function(response) {
-          return response.builds
-        })
-        .fail(function(err) {
-          ga('send', 'event', 'background', 'fetch_builds', 'error')
-          if (intercom) intercom.postMessage({type: 'error', data: err})
-        });
-    },
-
-    fetchProjects = function() {
-      if (options && options.api_key != undefined) {
-        var params = 'api_key=' + options.api_key
-
-        $.getJSON(PROJECT_URL, params)
+        $.getJSON(BUILD_URL, params)
           .done( function(response) {
-            return response.projects
-            if (intercom) intercom.postMessage({type: 'api_ok'})
+            builds_collection = new Builds(response.builds)
+            callback(builds_collection)
           })
           .fail(function(err) {
-            ga('send', 'event', 'background', 'fetch_projects', 'error')
+            ga('send', 'event', 'background', 'fetch_builds', 'error')
             if (intercom) intercom.postMessage({type: 'error', data: err})
           });
-      }
+      })
     },
 
-    fetchApiKeyFromLocalStorage = function() {
+    fetchProjects = function(callback) {
+      fetchApiKeyFromLocalStorage(function() {
+        if (options && options.api_key != undefined) {
+          var params = 'api_key=' + options.api_key
+          $.getJSON(PROJECT_URL, params)
+            .done( function(response) {
+              projects_collection = new Projects(response.projects)
+              callback(projects_collection)
+            })
+            .fail(function(err) {
+              ga('send', 'event', 'background', 'fetch_projects', 'error')
+            });
+        }
+      })
+    },
+
+    fetchApiKeyFromLocalStorage = function(callback) {
       chrome.storage.sync.get('api_key', function(value) {
         options = value
-        if (options) fetchProjectsFromCodeship()
+        callback()
       });
     }
 
   return {
-    initialize: function() {
-      fetchApiKeyFromLocalStorage();
+    fetchProjects: function(callback) {
+      fetchProjects(callback)
+    },
+
+    fetchBuilds: function(project, callback) {
+      fetchBuilds(project, callback)
     }
   }
 });
